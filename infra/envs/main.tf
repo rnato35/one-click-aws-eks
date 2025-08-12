6ccbea81e91a9e179938aeaf5000eda7973e6c4c
Renato Mendoza <renato@renatomendoza.io>
2025-08-12 16:48:39 -0500

    Rollback

--- infra/envs/main.tf
+++ infra/envs/main.tf
@@ -3,19 +3,19 @@
 ###############################
 
 # Keep it super simple so CI can apply without extra perms/features
- data "aws_caller_identity" "current" {}
+# data "aws_caller_identity" "current" {}
 
- locals {
+# locals {
 #   # Ensure bucket name is globally unique and DNS-compliant
-   bucket_name = lower(replace("${var.project_prefix}-${var.env_name}-${data.aws_caller_identity.current.account_id}-sample", "_", "-"))
- }
+#   bucket_name = lower(replace("${var.project_prefix}-${var.env_name}-${data.aws_caller_identity.current.account_id}-sample", "_", "-"))
+# }
 
- resource "aws_s3_bucket" "sample" {
-   bucket = local.bucket_name
-   tags   = local.tags
- }
+# resource "aws_s3_bucket" "sample" {
+#   bucket = local.bucket_name
+#   tags   = local.tags
+# }
 
- output "sample_bucket_name" {
-   value       = aws_s3_bucket.sample.bucket
-   description = "Minimal S3 bucket name for GitOps validation"
- }
+# output "sample_bucket_name" {
+#   value       = aws_s3_bucket.sample.bucket
+#   description = "Minimal S3 bucket name for GitOps validation"
+# }
